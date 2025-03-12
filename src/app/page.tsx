8929d9d30579df82573a0a89d284b79b5914c2bf
SeregaNag <serega_nag@tut.by>
2025-03-12 12:37:34 +0300

    Refactor order management replace CreateOrderButton with role-based button logic, add OrderCardList for displaying orders based on filter status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,16 @@ import OrderList from "@/components/OrderList";
 import RoleSwitch from "@/components/RoleSwitch";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
-import CreateOrderButton from "@/components/CreateOrderButton";
+import { use, useEffect, useState } from "react";
 
 export default function Home() {
   const role = useSelector((state: RootState) => state.user.role);
+  const [currentRole, setCurrentRole] = useState(role);
+
+  useEffect(() => {
+    setCurrentRole(role);
+  }
+  , [role]);
 
   return (
     <Container maxWidth="md" sx={{ textAlign: "center", mt: 5 }}>
@@ -18,16 +24,23 @@ export default function Home() {
       
       <RoleSwitch />
       
-      <Button 
+      {role === 'client' && <Button 
         variant="contained" 
         color="secondary"
         sx={{ mt: 3 }}
+        onClick={() => {
+          if (role === 'client') {
+            // создание заказа
+            console.log('Order created');
+          } else {
+            // фильтрация
+            console.log('Filtering orders');
+          }
+        }}
       >
-        {role === 'freelancer' ? 'Разместить заказ' : 'Показать задания'}
-      </Button>
-
-      {role === 'client' && <CreateOrderButton />}
-      <OrderList />
+        Разместить заказ
+      </Button>}
+      {currentRole === 'freelancer' && <OrderList />}
     </Container>
   );
 }
--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,12 +1,20 @@
 import { Container } from "@mui/material";
 import OrderCard from "./OrderCard";
 import OrderFilter from "./OrderFilter";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import OrderCardList from "./OrderCardList";
 
 export default function OrderList() {
+  const filter = useSelector((state: RootState) => state.filters.status);
   return (
     <Container>
       <OrderFilter />
-      <OrderCard />
+
+      {filter === "all" && <OrderCardList />}
+      {filter === "completed" && <OrderCardList />}
+      {filter === "pending" && <OrderCardList />}
+      {filter === "cancelled" && <OrderCardList />}
     </Container>
   );
 }
