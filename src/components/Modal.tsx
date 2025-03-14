a85520473196018086979c80b31e7018eea881a6
SeregaNag <serega_nag@tut.by>
2025-03-14 13:34:42 +0300

    Enhance Modal component with Fade transition for improved user experience

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { createPortal } from "react-dom"
-import { Box, Modal } from "@mui/material";
+import { Box, Modal, Fade } from "@mui/material";
 
 interface ModalProps {
     isOpen: boolean;
@@ -29,12 +29,14 @@ export default function ModalWindow({ isOpen, onClose, children }: ModalProps) {
       aria-describedby="modal-description"
       className="flex items-center justify-center"
     >
+      <Fade in={isOpen} timeout={1000}>
       <Box
         className="bg-white rounded-lg p-6 w-full max-w-md"
         onClick={(e) => e.stopPropagation()}
       >
         {children}
       </Box>
+      </Fade>
     </Modal>,
     document.body 
   );
